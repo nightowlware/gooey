4b9f1343f61e4ad5b200d2262f61852a15ca67c4
nightowlware <nightowlware@gmail.com>
2015-11-10 01:38:46 -0700

    Moved stuff from demo into gooey lib
    Moved all the gl and glfw code into gooey.go, now demo just uses gooey

--- gooey-demo/gooey-demo.go
+++ gooey-demo/gooey-demo.go
@@ -4,17 +4,10 @@ package main
 import (
     "fmt"
     "runtime"
-
     "github.com/nightowlware/gooey"
-
-    "github.com/go-gl/gl/v2.1/gl"
-    "github.com/go-gl/glfw/v3.1/glfw"
 )
 
-var screenwidth int = 640
-var screenheight int = 480
-
-// Needed for GLFW:
+// Needed for GLFW: TODO: can I move this into gooey package? not sure how...
 func init() {
     // This is needed to arrange that main() runs on main thread.
     // See documentation for functions that are only allowed to be called from the main thread.
@@ -27,75 +20,12 @@ func main() {
     gooey.Init()
     defer gooey.Terminate()
 
-    w := gooey.CreateWindow(screenwidth, screenheight, "Gooey Demo v0.1")
-
-    window := w.Handle()
-
-    window.MakeContextCurrent()
-
-    err := gl.Init();
-    if err != nil {
-        panic(err)
-    }
-
+    window := gooey.CreateWindow(640, 480, "Gooey Demo v0.1")
 
     e := new(gooey.Element)
     fmt.Println(e)
 
-
-
-
-    setupScene()
-    for !window.ShouldClose() {
-        drawScene()
-        window.SwapBuffers()
-        glfw.PollEvents()
-    }
-
+    gooey.Run(window)
 
     fmt.Println("Goodbye!")
-}
-
-
-func setupScene() {
-    gl.Disable(gl.DEPTH_TEST)
-    gl.Disable(gl.LIGHTING)
-
-    gl.ClearColor(0.5, 0.5, 0.5, 0.0)
-
-    gl.MatrixMode(gl.PROJECTION)
-    gl.LoadIdentity()
-    gl.Ortho(0, float64(screenwidth), 0, float64(screenheight), -1, 1)
-    gl.MatrixMode(gl.MODELVIEW)
-    gl.LoadIdentity()
-}
-
-func drawScene() {
-    gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-
-    gl.LineWidth(1)
-
-    gl.MatrixMode(gl.MODELVIEW)
-    gl.LoadIdentity()
-    gl.Translatef(0, 0, 0.0)
-    //gl.Rotatef(rotationX, 1, 0, 0)
-    //gl.Rotatef(rotationY, 0, 1, 0)
-
-    //rotationX += 0.5
-    //rotationY += 0.5
-
-    //gl.BindTexture(gl.TEXTURE_2D, texture)
-
-    gl.Color4f(1, 0, 0, 1)
-
-    gl.Begin(gl.QUADS)
-
-    gl.Vertex3f(50, 50, 0)
-    gl.Vertex3f(50, 100, 0)
-    gl.Vertex3f(100, 100, 0)
-    gl.Vertex3f(100, 50, 0)
-
-    gl.End()
-
-
 }
--- gooey.go
+++ gooey.go
@@ -2,8 +2,9 @@
 package gooey
 
 import (
-"fmt"
-"github.com/go-gl/glfw/v3.1/glfw"
+    "fmt"
+    "github.com/go-gl/gl/v2.1/gl"
+    "github.com/go-gl/glfw/v3.1/glfw"
 )
 
 type Element struct {
@@ -59,12 +60,16 @@ func Terminate() {
 
 type Window struct {
     handle *glfw.Window
+
+    width int
+    height int
 }
 
 func (w *Window) Handle() *glfw.Window {
     return w.handle
 }
 
+//TODO: how to handle multiple windows? Do we even want to allow that?
 func CreateWindow(width int, height int, title string) *Window {
     handle, err := glfw.CreateWindow(width, height, title, nil, nil)
     if err != nil {
@@ -73,7 +78,68 @@ func CreateWindow(width int, height int, title string) *Window {
 
     w := new(Window)
     w.handle = handle
+    w.width = width
+    w.height = height
+
+    handle.MakeContextCurrent()
+
+    err = gl.Init();
+    if err != nil {
+        panic(err)
+    }
+
     return w
 }
 
+func Run(window *Window) {
+    setupScene(window.width, window.height)
+    for !window.handle.ShouldClose() {
+        drawScene()
+        window.handle.SwapBuffers()
+        glfw.PollEvents()
+    }
+}
 
+
+func setupScene(width int, height int) {
+    gl.Disable(gl.DEPTH_TEST)
+    gl.Disable(gl.LIGHTING)
+
+    gl.ClearColor(0.5, 0.5, 0.5, 0.0)
+
+    gl.MatrixMode(gl.PROJECTION)
+    gl.LoadIdentity()
+    gl.Ortho(0, float64(width), 0, float64(height), -1, 1)
+    gl.MatrixMode(gl.MODELVIEW)
+    gl.LoadIdentity()
+}
+
+func drawScene() {
+    gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+    gl.LineWidth(1)
+
+    gl.MatrixMode(gl.MODELVIEW)
+    gl.LoadIdentity()
+    gl.Translatef(0, 0, 0.0)
+    //gl.Rotatef(rotationX, 1, 0, 0)
+    //gl.Rotatef(rotationY, 0, 1, 0)
+
+    //rotationX += 0.5
+    //rotationY += 0.5
+
+    //gl.BindTexture(gl.TEXTURE_2D, texture)
+
+    gl.Color4f(1, 0, 0, 1)
+
+    gl.Begin(gl.QUADS)
+
+    gl.Vertex3f(50, 50, 0)
+    gl.Vertex3f(50, 100, 0)
+    gl.Vertex3f(100, 100, 0)
+    gl.Vertex3f(100, 50, 0)
+
+    gl.End()
+
+
+}
